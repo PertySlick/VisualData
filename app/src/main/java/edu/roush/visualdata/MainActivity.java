eebb53de78b685f13853f6a5d567ffa364d99f9d
Tim Roush <perty_slick@hotmail.com>
2017-11-02 19:32:07 -0700

    Created Vehicle and Owner models

--- app/src/main/java/edu/roush/visualdata/MainActivity.java
+++ app/src/main/java/edu/roush/visualdata/MainActivity.java
@@ -3,11 +3,20 @@ package edu.roush.visualdata;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String testData = "";
+        try {
+            JSONObject testObject = new JSONObject(testData);
+        } catch (Exception e) {
+
+        }
     }
 }
---
+++ app/src/main/java/edu/roush/visualdata/model/Owner.java
@@ -0,0 +1,8 @@
+package edu.roush.visualdata.model;
+
+/**
+ * Created by Perty on 11/2/2017.
+ */
+
+public class Owner {
+}
